11c60f48c6007f5dc93de35fb163a1985960ac8e
Lei Yang <lyang@msrb.org>
2022-07-07 12:04:14 -0400

    Fix compile error

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,7 @@
-import { Container, Link } from '@mui/material'
+import { Container } from '@mui/material'
 import React from 'react'
-import { Auth } from 'aws-amplify'
 import '@aws-amplify/ui-react/styles.css'
-import { withAuthenticator, Button as LoginButton, Heading, Authenticator } from '@aws-amplify/ui-react'
-import router from 'next/router'
+import { Authenticator } from '@aws-amplify/ui-react'
 const login = () => {
   return (
     <Container>
